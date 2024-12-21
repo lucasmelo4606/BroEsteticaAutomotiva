b67f6a4cc56814ca743f59b3600dfd19e77852b0
Adelsondev <adelsonmarques10@gmail.com>
2024-12-20 21:43:10 -0300

    alterado a ordem dos pacotes

--- src/components/Pacotes/Pacotes.jsx
+++ src/components/Pacotes/Pacotes.jsx
@@ -3,6 +3,34 @@ import "./pacotes.css";
 export const Pacotes = () => {
   return (
     <div className="pacotes">
+      <div className="pacote">
+      <h2>PACOTE BASIC</h2>
+      <ul>
+        <li>Tratamento interno</li>
+        <li>Tratamento dos plásticos internos e externos</li>
+        <li>Correção de pintura</li>
+        <li>Proteção de 1 Ano</li>
+      </ul>
+      <div className="botao">
+        <ButtonHome BHome="SABER MAIS" />
+      </div>
+    </div>
+
+    <div className="pacote">
+      <h2>PACOTE BUSINESS</h2>
+      <ul>
+        <li>Tratamento interno</li>
+        <li>Tratamento dos vidros</li>
+        <li>Remoção de chuva ácida</li>
+        <li>Tratamento dos plásticos internos e externos</li>
+        <li>Correção de pintura</li>
+        <li>Proteção de 3 Anos</li>
+      </ul>
+      <div className="botao">
+        <ButtonHome BHome="SABER MAIS" />
+      </div>
+    </div>
+
     <div className="pacote">
       <h2>PACOTE BRO</h2>
       <ul>
@@ -20,33 +48,9 @@ export const Pacotes = () => {
       </div>
     </div>
   
-    <div className="pacote">
-      <h2>PACOTE BUSINESS</h2>
-      <ul>
-        <li>Tratamento interno</li>
-        <li>Tratamento dos vidros</li>
-        <li>Remoção de chuva ácida</li>
-        <li>Tratamento dos plásticos internos e externos</li>
-        <li>Correção de pintura</li>
-        <li>Proteção de 3 Anos</li>
-      </ul>
-      <div className="botao">
-        <ButtonHome BHome="SABER MAIS" />
-      </div>
-    </div>
+   
   
-    <div className="pacote">
-      <h2>PACOTE BASIC</h2>
-      <ul>
-        <li>Tratamento interno</li>
-        <li>Tratamento dos plásticos internos e externos</li>
-        <li>Correção de pintura</li>
-        <li>Proteção de 1 Ano</li>
-      </ul>
-      <div className="botao">
-        <ButtonHome BHome="SABER MAIS" />
-      </div>
-    </div>
+    
   </div>
   
   );
