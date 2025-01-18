c937b4a5cc1ce333721e58db1cb466a2711e0b5b
lucasmelo4606 <lucasmelo4606@gmail.com>
2025-01-17 21:08:03 -0300

    teste

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Banner } from "../../components/Banner/Banner"
 import { Cards } from "../../components/Cards/Cards"
 import { Pacotes } from "../../components/Pacotes/Pacotes"
 import { Info } from "../../components/informacoes/Info"
-import { Topicos } from "../../components/Topico/topicos"
+// import {Topicos} from "../../components/Topico/topicos"
 import { Rodape } from "../../components/rodape/Rodape"
 import { Nav } from "../../components/Nav/Nav"
 
@@ -12,17 +12,14 @@ export const Home = () =>{
 
     return(
         <>
-        <Nav />
-        <Banner />
-        <Topicos txtTopico={'Pacotes'}/>
-        <Pacotes />
-        <Topicos txtTopico={"Nossos Serviços Automotivos"} />
-        <Cards />
-        <Info />
-        <Rodape />
-        
-        
-        
+            <Nav />
+            <Banner />
+            <Topicos txtTopico={'Pacotes'}/>
+            <Pacotes />
+            {/* <Topicos txtTopico={"Nossos Serviços Automotivos"} /> */}
+            <Cards />
+            <Info />
+            <Rodape />
         </>
 
 
